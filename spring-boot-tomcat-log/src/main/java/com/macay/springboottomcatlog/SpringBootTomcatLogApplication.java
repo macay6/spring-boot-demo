ec8604e22eec9ba8ebcd8030a7421158617cbb6b
macay <1207260521@qq.com>
2020-12-18 23:37:21 +0800

    tomcat日志配置

---
+++ spring-boot-tomcat-log/src/main/java/com/macay/springboottomcatlog/SpringBootTomcatLogApplication.java
@@ -0,0 +1,13 @@
+package com.macay.springboottomcatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTomcatLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatLogApplication.class, args);
+    }
+
+}
---
+++ spring-boot-tomcat-log/src/main/java/com/macay/springboottomcatlog/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.macay.springboottomcatlog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: HelloController
+ * @Description:
+ * @Author: Macay
+ * @Date: 2020/12/18 10:29 下午
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello tomcat";
+    }
+}
